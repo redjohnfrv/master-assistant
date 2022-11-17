268a5948a9c68f2de43ec6ba893be3072b1d5d13
artaina117 <artaina117@gmail.com>
2022-11-18 00:02:22 +0600

    feature: notion and rolldice components

---
+++ src/components/Notion/Notion.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Notion = () => {
+  return (
+    <div>
+      Notion
+    </div>
+  )
+}
--- src/screen/components/Content/Content.tsx
+++ src/screen/components/Content/Content.tsx
@@ -1,1 +1,38 @@
-import React, {BaseSyntheticEvent, useState} from 'react'import {FormType, RenderVariant} from '../../../types'import {List, SetTimer} from '../../../components'import {render} from './utils'import {UseFormRegister} from 'react-hook-form'import {ContentLayout} from '../ContentLayout/ContentLayout'type Props = {  register: UseFormRegister<FormType>  onTimerSubmit: (e?: BaseSyntheticEvent | undefined) => Promise<void>}export const Content = (props: Props) => {  const {register, onTimerSubmit} = props  const onSetRenderElement = (variant: RenderVariant) => {    if (variant === 'list') {      setEl(<List onClick={onSetRenderElement}/>)    }    if (variant === 'timer') {      setEl(<SetTimer register={register} onTimerSubmit={onTimerSubmit}/>)    }  }  const [el, setEl] = useState<JSX.Element>(<List onClick={onSetRenderElement}/>)  return render (    <ContentLayout onSetRenderElement={onSetRenderElement}>      {el}    </ContentLayout>  )}
+import React, {BaseSyntheticEvent, useState} from 'react'
+import {FormType, RenderVariant} from '../../../types'
+import {List, Notion, RollDice, SetTimer} from '../../../components'
+import {render} from './utils'
+import {UseFormRegister} from 'react-hook-form'
+import {ContentLayout} from '../ContentLayout/ContentLayout'
+
+type Props = {
+  register: UseFormRegister<FormType>
+  onTimerSubmit: (e?: BaseSyntheticEvent | undefined) => Promise<void>
+}
+
+export const Content = (props: Props) => {
+  const {register, onTimerSubmit} = props
+
+  const onSetRenderElement = (variant: RenderVariant) => {
+    if (variant === 'list') {
+      setEl(<List onClick={onSetRenderElement}/>)
+    }
+    if (variant === 'timer') {
+      setEl(<SetTimer register={register} onTimerSubmit={onTimerSubmit}/>)
+    }
+    if (variant === 'dice') {
+      setEl(<RollDice />)
+    }
+    if (variant === 'notion') {
+      setEl(<Notion />)
+    }
+  }
+
+  const [el, setEl] = useState<JSX.Element>(<List onClick={onSetRenderElement}/>)
+
+  return render (
+    <ContentLayout onSetRenderElement={onSetRenderElement}>
+      {el}
+    </ContentLayout>
+  )
+}
