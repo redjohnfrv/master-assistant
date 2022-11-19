57aad22cce87e79651b205884f71418abbca2058
R <ivan.js.style@gmail.com>
2022-11-19 14:13:01 +0600

    styles: back button

--- src/layout/Block/Block.tsx
+++ src/layout/Block/Block.tsx
@@ -19,7 +19,7 @@ export const Block = (props: Props) => {
 const Root = styled.div<{$radius: number}>`
   width: max-content;
   padding: 6px 12px;
-  box-shadow: rgba(50, 50, 93, 0.25) 0 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  border: 1px solid #fff;
   border-radius: ${({$radius}) => $radius}px;
   color:  ${({theme: {colors}}) => colors.grayscale[0]};
   background: ${({theme: {colors}}) => colors.primary[300]};
